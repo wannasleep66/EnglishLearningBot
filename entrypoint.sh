93009fafc07422ba5f217dd857e08735a588fc6b
Daniil <daniil.karpenk0@yandex.ru>
2025-05-13 18:04:34 +0500

    Добавлен пример .env файла

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-
-echo "Waiting for database"
-while ! nc -z db 5432; do
-  sleep 0.1
-done
-echo "Database is up"
-
 echo "Applying migrations"
-alembic upgrade head
+
+poetry run alembic upgrade head
 
 echo "Running seeds"
-python app/seed.py
+poetry run python src/database/seeds/__init__.py
 
 exec "$@"
